36a1188049507a929400171c24e8f31391fc2166
stevenfrio <johnstevenfrio@yahoo.com>
2025-05-31 17:54:07 +0800

    Initial Commit

---
+++ Rose Bedsheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rose Bedsheet
+//
+//  Created by John Steven Frio on 5/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
