c7d8e8e708c565e9da9fe7046ba079566d38b74f
stevenfrio <johnstevenfrio@yahoo.com>
2025-06-11 08:55:07 +0800

    style: updated homeview ui

--- Rose Bedsheet/Modules/Home/HomeView.swift
+++ Rose Bedsheet/Modules/Home/HomeView.swift
@@ -14,14 +14,24 @@ struct HomeView: View {
             headerView
             buttonStackView
             
-        }
+            HStack(spacing: 4) {
+                Text("Already have an account?")
+                    .applyTypography(.bodyRegularLeading)
+                    .foregroundStyle(AppColors.secondaryText)
+                    .fixedSize()
+                AppButton("Sign in", config: .init(style: .textOnly)) {
+                }
+                .fixedSize()
+            }
+        }.padding(.horizontal, 16)
     }
     
     private var headerView: some View {
         VStack(spacing: 24) {
-            BaseImage(image: UIImage(systemName: "bed.double.circle.fill") ?? UIImage())
+            BaseImage(imageString: "bed.double.circle.fill")
                 .foregroundStyle(AppColors.primary)
                 .frame(width: 100, height: 100)
+            
             VStack(spacing: 16) {
                 Text("Welcome to \(Constants.app_name)")
                     .applyTypography(.largeTitleBoldCenter)
@@ -35,7 +45,43 @@ struct HomeView: View {
     
     private var buttonStackView: some View {
         VStack {
+            let config = AppCellTypographyConfiguration(
+                titleTypography: .bodySemiboldLeading,
+                titleColor: AppColors.primaryText,
+                subTitleTypography: .bodyRegularLeading,
+                subtitleColor: AppColors.secondaryText)
             
+            AppCell(
+                title: "Become a Seller",
+                subTitle: "List and sell your bedsheets, pillowcases and blankets",
+                typographyConfig: config,
+                leftView: {
+                    Image(systemName: "storefront")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(AppColors.primary)
+                },
+                rightView: {
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                }
+            )
+            
+            AppCell(
+                title: "Become a Buyer",
+                subTitle: "Browse and shop quality bedsheets, pillowcases and blankets",
+                typographyConfig: config,
+                leftView: {
+                    Image(systemName: "handbag")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(AppColors.primary)
+                },
+                rightView: {
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                }
+            )
         }
     }
 }
